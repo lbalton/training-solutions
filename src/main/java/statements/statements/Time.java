0aeef17429e596589caec1a826afc047f6e310b2
lbalton <lbalton@hotmail.com>
2020-11-07 16:28:40 +0100

    Time/TimeMain

---
+++ src/main/java/statements/statements/Time.java
@@ -0,0 +1,27 @@
+package statements.statements;
+
+public class Time {
+    private int hour;
+    private int minute;
+    private int second;
+
+    public Time(int hour, int minute, int second) {
+        this.hour = hour;
+        this.minute = minute;
+        this.second = second;
+    }
+
+    public int getInMinutes(){
+        int hourInMinute = 60;
+        return hour * hourInMinute + minute;
+    }
+    public int getInSecond(){
+        int minuteInSecond = 60;
+        return minute * minuteInSecond + second;
+    }
+   public String toString(){
+       return hour+" : " + minute + " : " + second;
+   }
+
+
+}
---
+++ src/main/java/statements/statements/TimeMain.java
@@ -0,0 +1,19 @@
+package statements.statements;
+
+import java.util.Scanner;
+
+public class TimeMain {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Hány óra?");
+        int firstHour = scanner.nextInt();
+        System.out.println("Hány perce?");
+        int firstMinute = scanner.nextInt();
+        System.out.println("Hány másodtpercet?");
+        int firstSecold = scanner.nextInt();
+
+        Time time = new Time(firstHour, firstMinute, firstSecold);
+
+        System.out.println("Első időpont: " + time.toString() +" Az = " + time.getInMinutes()+ " perc");
+    }
+}
