879fdd430b096e734ff029f20bd393f2cb9358c2
lbalton <lbalton@hotmail.com>
2020-11-08 21:53:55 +0100

    IntroControl/IntroControlMain SNAPSHOT

---
+++ src/main/java/introcontrol/introcontrol/IntroControl.java
@@ -0,0 +1,42 @@
+package introcontrol.introcontrol;
+
+public class IntroControl {
+
+
+    public int substractTenIfGreaterThanTen(int number){
+          if (number <= 10){
+             return number;
+       }
+          else {
+             return  number-10;
+       }
+    }
+
+    public String describeNumber(int number){
+        if (number == 0){
+            return "Zero";
+        }
+        else {
+            return "Not Zero";
+        }
+    }
+
+        public String greetingToJoe(String name){
+            if (name.equals("Joe")){
+                return "Hello Joe";
+            }
+            else {
+                return "";
+        }
+    }
+    public int calculateBonus(int sale){
+        return
+    }
+
+
+
+
+
+}
+
+
---
+++ src/main/java/introcontrol/introcontrol/IntroControlMain.java
@@ -0,0 +1,18 @@
+package introcontrol.introcontrol;
+
+import introcontrol.introcontrol.IntroControl;
+
+public class IntroControlMain {
+    public static void main(String[] args) {
+        IntroControl introControl = new IntroControl();
+
+        System.out.println(introControl.substractTenIfGreaterThanTen(11));
+
+        System.out.println(introControl.describeNumber(10));
+
+        System.out.println(introControl.greetingToJoe("Joe"));
+
+
+
+    }
+}
