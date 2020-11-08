521b12f3d78c264756cfc219bac558572c98e00e
lbalton <lbalton@hotmail.com>
2020-11-08 12:57:33 +0100

    Investment/InvestMain

---
+++ src/main/java/statements/statements/Investment.java
@@ -0,0 +1,29 @@
+package statements.statements;
+
+public class Investment {
+    private double cost;
+    private int fund;
+    private int interestRate;
+    private boolean active;
+
+
+    public Investment(int fund,int interestRate){
+        this.fund = fund;
+        this.interestRate = interestRate;
+    }
+    public int getFund(){
+        return fund;
+    }
+
+    public double getYield(int days){
+        return fund * interestRate * days / (100.0* 365);
+
+    }
+    public double close(int days) {
+        double totalAmount = (getFund() + getYield(days)) * (1 - cost / 100);
+        double payout = active ? totalAmount : 0;
+        active = false;
+        return payout;
+    }
+
+}
