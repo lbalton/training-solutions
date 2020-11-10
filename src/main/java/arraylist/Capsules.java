3384629e2da3b4921bba3fb4820ca49aa4af159e
lbalton <lbalton@hotmail.com>
2020-11-10 20:31:49 +0100

    Capsules // snapshot

---
+++ src/main/java/arraylist/Capsules.java
@@ -0,0 +1,32 @@
+package arraylist;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class Capsules {
+
+    private List<String> color = new ArrayList<>();
+    private String ArrayList;
+
+    private String addLast;
+
+    public void addLast(String) {
+
+        };
+    public void addFirst(String) {
+
+        };
+    public void removeFirst() {
+
+        };
+    public void removeLast() {
+
+        };
+
+    public static void main(String[] args) {
+
+    }
+}
+
+
+
