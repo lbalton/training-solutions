fd60d9150ed16325ac17eaa8611ec65fb6b064f7
lbalton <lbalton@hotmail.com>
2020-11-11 20:59:11 +0100

    Books // Snapshot

---
+++ src/main/java/Arraymain/Books.java
@@ -0,0 +1,15 @@
+package Arraymain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Books {
+
+    private List<String> bookTitle = new ArrayList<>();
+
+    public void add(String title) {
+        bookTitle.add(title);
+    }
+
+
+}
