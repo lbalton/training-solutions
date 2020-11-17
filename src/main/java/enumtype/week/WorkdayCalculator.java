4aab0494bfd7962077c9f749a99834a92122787d
lbalton <lbalton@hotmail.com>
2020-11-17 13:06:31 +0100

    Day/DayType // snapshot

---
+++ src/main/java/enumtype/week/WorkdayCalculator.java
@@ -0,0 +1,10 @@
+package enumtype.week;
+
+import java.util.List;
+
+public class WorkdayCalculator {
+
+   private Day nextDay;
+
+
+}
--- src/main/java/intromethod/TodoList.java
+++ src/main/java/intromethod/TodoList.java
@@ -14,39 +14,4 @@ public class TodoList {
 
     public void finishTodos(String caption){
         todos.remove(caption);
-    }
-
-
-
-}
-
-
-
-
-    /*
-    Todo todo;
-
-    private List<String> todos = new ArrayList<String>();
-
-    public String addTodo(String caption){
-        todos.add(caption)
-    }
-
-    public void finishTodo(String caption){
-        if (todo.getCaption().equals(caption)){
-
-        }
-    }
-
-    public String finishAllTodos(List<String> todosToFinish){
-
-    }
-
-    public int numberOfFinishedTodos(){
-
-    }
-
-
-
-}
-*/
+    }}
