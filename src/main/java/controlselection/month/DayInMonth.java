c1e4b70e146282b418a8a580e83327600bd67cf7
lbalton <lbalton@hotmail.com>
2020-11-20 21:06:08 +0100

    DayInMonth

---
+++ src/main/java/controlselection/month/DayInMonth.java
@@ -0,0 +1,45 @@
+package controlselection.month;
+
+public class DayInMonth {
+
+
+
+
+    public String DayInTheMonth(String nameOfMonth) {
+
+        nameOfMonth.toLowerCase();
+        String numberOfDayInMonth;
+
+        switch (nameOfMonth){
+            case "január":
+            case "március":
+            case "május":
+            case "július":
+            case "augusztus":
+            case "október":
+            case "december":
+                numberOfDayInMonth = "Ez a hónap 31 napos!";
+                break;
+
+            case "április":
+            case "június":
+            case "szeptember":
+            case "november":
+                numberOfDayInMonth = "Ez a hónap 30 napos!";
+                break;
+
+            case "február":
+                numberOfDayInMonth = " Szökőévtől föggően 28 vagy 29 napos ";
+                break;
+            default:
+                throw new IllegalArgumentException("Invalid month: " + nameOfMonth);
+        }
+
+            return  numberOfDayInMonth;
+
+    }
+
+
+    }
+
+
