5a028a155b35eb5fe57b6756712d2284f5a4df35
lbalton <lbalton@hotmail.com>
2020-11-26 21:49:24 +0100

    Project 0.1 //snapshot

---
+++ src/main/java/Project1/Controller.java
@@ -0,0 +1,30 @@
+package Project1;
+
+import java.util.Scanner;
+
+public class Controller {
+
+    Office office = new Office();
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Hány tárgyalót kíván rögzíteni?");
+
+        int numberOfRooms = scanner.nextInt();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ src/main/java/Project1/Office.java
@@ -0,0 +1,15 @@
+package Project1;
+
+import java.util.List;
+
+public class Office {
+
+    List<MeetingRoom> meetingRooms;
+
+    //public List addMeetingRoom(MeetingRoom meetingRoom){
+
+    //  }
+
+
+
+}
