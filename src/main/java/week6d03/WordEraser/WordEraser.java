f55135d2254d5b421c9b42ebd706ad1349de8324
lbalton <lbalton@hotmail.com>
2020-12-05 19:18:45 +0100

    WordEraser

---
+++ src/main/java/week6d03/WordEraser/WordEraser.java
@@ -0,0 +1,19 @@
+package week6d03.WordEraser;
+
+public class WordEraser {
+
+
+
+    private String[] arrWords;
+    public String eraseWord(String words, String word){
+        StringBuilder builder = new StringBuilder();
+        String[] arrWords = words.split(" ");
+        for (int i = 0;i < arrWords.length ;i++){
+            if ((!arrWords[i].equals(word))){
+                builder.append(arrWords[i]+ " ");
+            }
+        }
+        return builder.toString().trim();
+    }
+
+}
