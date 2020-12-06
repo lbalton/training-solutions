6d2fcb4c3ce5dc8f2ca38bba71096088b3b1c57c
lbalton <lbalton@hotmail.com>
2020-12-06 17:45:21 +0100

    Dog/Human

---
+++ src/main/java/javabeans/Human.java
@@ -0,0 +1,33 @@
+package javabeans;
+
+public class Human {
+
+    private String name;
+    private Double weight;
+    private int iq;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(Double weight) {
+        this.weight = weight;
+    }
+
+    public int getIq() {
+        return iq;
+    }
+
+    public void setIq(int iq) {
+        this.iq = iq;
+    }
+}
