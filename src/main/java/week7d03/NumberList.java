ea728ad2c85c8684e495336b5beceb6e3dd79ae2
lbalton <lbalton@hotmail.com>
2020-12-10 19:47:01 +0100

    NumberList

---
+++ src/main/java/week7d03/NumberList.java
@@ -0,0 +1,38 @@
+package week7d03;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NumberList {
+
+
+    public boolean isIncreasing(List<Integer> numbersList){
+
+
+      for (int i = 1; i < numbersList.size() ; i++){
+          if (numbersList.get(i-1)> numbersList.get(i)){
+              return  false;
+          }
+      }
+      return true;
+    }
+
+    public static void main(String[] args) {
+        NumberList numberList = new NumberList();
+
+        List<Integer> numbersLi =  new ArrayList<>();
+        numbersLi.add(1);
+        numbersLi.add(3);
+        numbersLi.add(3);
+        numbersLi.add(7);
+        numbersLi.add(111);
+        numbersLi.add(3000);
+
+        System.out.println(numberList.isIncreasing(numbersLi));
+
+
+    }
+
+
+}
