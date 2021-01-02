eace7b9378724ba000db95d543d5588e662b26ea
lbalton <lbalton@hotmail.com>
2021-01-02 12:54:52 +0100

    Lotto

---
+++ src/main/java/week8/week08d02/Lotto/Lotto.java
@@ -0,0 +1,42 @@
+package week8.week08d02.Lotto;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class Lotto {
+
+    public List<Integer> randomNumbers(){
+
+
+        List<Integer> Lotto = new ArrayList<>();
+
+        for(int x = 0; x < 5 ; x++){
+            int number = new Random().nextInt(91);
+            if(!Lotto.contains(number)){
+                Lotto.add(number);
+            }
+            else if (Lotto.contains(number)){
+                for (int i = 0; i < Lotto.size() ;i++){
+                    if (Lotto.indexOf(i) == number){
+                        Lotto.remove(i);
+                    }
+                }
+                Lotto.add(new Random().nextInt(91));
+            }
+        }
+        return  Lotto;
+    }
+
+
+   public static void main(String[] args) {
+
+        Lotto lotto = new Lotto();
+
+        System.out.println(lotto.randomNumbers());
+
+        System.out.println(lotto.randomNumbers().size());
+
+    }
+}
+
