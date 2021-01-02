5159a1da09a74055673c7dbbb3fbf56788ec83d1
lbalton <lbalton@hotmail.com>
2021-01-02 16:24:53 +0100

    NumberGuesser

---
+++ src/main/java/week4/NumberGuesser.java
@@ -0,0 +1,56 @@
+package week4;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class NumberGuesser {
+
+
+    public void WhatIsTheNumber() {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int number = new Random().nextInt(101);
+
+        int isItThatNumber;
+
+
+        System.out.println("In this game, you have to find my number between 0-100.");
+        System.out.println("GOOD LUCK!");
+
+        System.out.println(" ");
+        System.out.println("I think a number!");
+
+        isItThatNumber = scanner.nextInt();
+
+        while (number != isItThatNumber) {
+
+
+            if (number < isItThatNumber) {
+                System.out.println("This is too much");
+                isItThatNumber = scanner.nextInt();
+
+            } else if (number > isItThatNumber) {
+                System.out.println("This is too less!");
+                isItThatNumber = scanner.nextInt();
+            }
+        }
+
+        if (number == isItThatNumber) {
+            System.out.println("Yes, It was my number, good job!");
+        }
+
+    }
+
+
+    public static void main(String[] args) {
+
+        NumberGuesser numberGuesser = new NumberGuesser();
+
+        numberGuesser.WhatIsTheNumber();
+
+
+    }
+}
+
+
