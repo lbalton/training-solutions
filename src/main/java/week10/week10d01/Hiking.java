b34a7a8e637297f1c596be46422b47b65895c048
lbalton <lbalton@hotmail.com>
2021-01-09 23:13:29 +0100

    Hiking

---
+++ src/main/java/week10/week10d01/Hiking.java
@@ -0,0 +1,38 @@
+package week10.week10d01;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Hiking {
+
+
+
+   public double getPlusElevation(List<Double> High){
+
+       double sum = 0;
+       for (int x = 1; x < High.size() ;x++){
+          if (High.get(x) > High.get(x-1));
+          sum += High.get(x)-High.get(x-1);
+       }
+
+       return sum;
+
+    }
+
+    public static void main(String[] args) {
+        Hiking hiking = new Hiking();
+
+        List<Double>TestList = new ArrayList<>();
+
+        TestList.add(12.2);
+        TestList.add(21.2);
+        TestList.add(32.2);
+        TestList.add(2.2);
+        TestList.add(2.5);
+        TestList.add(3.4);
+
+        System.out.println(hiking.getPlusElevation(TestList));
+
+    }
+
+}
