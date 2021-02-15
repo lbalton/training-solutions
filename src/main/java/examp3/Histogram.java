95dd6aa2e0abe557a9ab9c58a5560361dd2f20ef
Lengyel Bálint <lbalton@hotmail.com>
2021-02-15 19:17:34 +0100

    Histogram

---
+++ src/main/java/examp3/Histogram.java
@@ -0,0 +1,50 @@
+package examp3;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+
+public class Histogram {
+
+
+    public String createHistogram(BufferedReader bf){
+
+        String star = "*";
+
+        StringBuilder giveBack = new StringBuilder();
+
+        try {
+            String line;
+            while ((line = bf.readLine()) != null) {
+
+                int number = Integer.parseInt(line);
+
+                giveBack.append(star.repeat(number) + "\n");
+
+            }
+
+        } catch (IOException ioe) {
+            throw new IllegalStateException("Can't read the file", ioe);
+        }
+        return giveBack.toString();
+    }
+
+
+    public static void main(String[] args) {
+
+        Histogram histogram = new Histogram();
+
+        try (BufferedReader br = Files.newBufferedReader(Path.of("histogram.txt"))) {
+
+            System.out.println(histogram.createHistogram(br));
+
+        } catch (IOException iae) {
+            throw new IllegalArgumentException("Cant loading file", iae);
+        }
+
+    }
+
+}
